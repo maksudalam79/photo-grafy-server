adfe1b1f6dfa15e04fd66ce8024e513a49da1a71
maksudalam <maksud.cdz@gmail.com>
2022-11-08 12:55:54 +0600

    database connection complite

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require ("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors=require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app=express();
 const port=process.env.PORT||5000;
@@ -10,7 +10,15 @@ app.use(cors())
 app.use(express.json())
 
 // mongodb:---
+const uri =`mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.2q6mq2n.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
 // testing:---
